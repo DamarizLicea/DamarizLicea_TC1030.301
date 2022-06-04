51aa0ae5f05081c9b6bf605485c7ace71fccbbf4
DamarizLicea <89052608+DamarizLicea@users.noreply.github.com>
2022-06-03 21:15:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "Pasteleria.h"
+#include "Producto.h"
+using namespace std;
+ int main( ){
+     int dept=0;
+     Producto * arg1= new Pastel;
+     Producto * arg2= new Tarta;
+     Producto * arg3= new Galleta;
+     
 
+     Pasteleria pasteleria;
+     pasteleria.show_pedidos();
+     pasteleria.dinero_entrante_peds_portipo();
+     pasteleria.prueba_pedidos();
+
+     cout<<"ahora si hay cpp, prueba de funcionamiento:   \n\n";
+     pasteleria.show_pedidos("Pastel");
+     dept=pasteleria.dinero_entrante_peds_portipo();
+     cout<<"Dinero que ha entrado hoy"<<dept<<endl;
+
+ }
